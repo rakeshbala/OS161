d58719f112ad83cf69b815b29bbc72d4c749b545
Rakesh Balasubramanian <rbalasub@buffalo.edu>
2015-03-02 19:01:32 -0500

    Fixed fork declaration

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -67,7 +67,7 @@ int sys_chdir(userptr_t pathname);
 int sys___getcwd(userptr_t buf, size_t buflen,size_t *data_len);
 void sys__exit(int exitcode);
 int sys_waitpid(int pid, userptr_t status, int options, pid_t *ret_pid);
-int sys_fork(struct trapframe *tf);
+int sys_fork(struct trapframe *tf, pid_t *ret_pid);
 int sys_dup2(int oldfd, int newfd, int *ret_fd);
 
 #endif /* _SYSCALL_H_ */
