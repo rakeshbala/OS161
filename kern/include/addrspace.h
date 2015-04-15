2f16d78a2fdb1d81e7bd41c096c619760f7aed16
ops-class.org Student <trinity@zion.(none)>
2015-04-15 10:33:10 -0400

    removed dumbvm portions in vm.c

--- kern/include/addrspace.h
+++ kern/include/addrspace.h
@@ -91,13 +91,13 @@ struct addrspace {
 
         vaddr_t stack_end;
 
-        vaddr_t as_vbase1;
-        paddr_t as_pbase1;
-        size_t as_npages1;
-        vaddr_t as_vbase2;
-        paddr_t as_pbase2;
-        size_t as_npages2;
-        paddr_t as_stackpbase;
+        // vaddr_t as_vbase1;
+        // paddr_t as_pbase1;
+        // size_t as_npages1;
+        // vaddr_t as_vbase2;
+        // paddr_t as_pbase2;
+        // size_t as_npages2;
+        // paddr_t as_stackpbase;
 #endif
 };
 
