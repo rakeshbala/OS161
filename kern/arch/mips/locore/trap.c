7f52cb744596d39fe8953aafbda7cace9b0c6ee5
ops-class.org Student <trinity@zion.(none)>
2015-04-18 17:56:54 -0400

    trap.c comments

--- kern/arch/mips/locore/trap.c
+++ kern/arch/mips/locore/trap.c
@@ -114,11 +114,6 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 		break;
 	}
 
-	/*
-	 * You will probably want to change this.
-	 */
-
-
 	 /************ RB:Implemented graceful exit ************/
 	kprintf("User mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
 		code, sig, trapcodenames[code], epc, vaddr);
